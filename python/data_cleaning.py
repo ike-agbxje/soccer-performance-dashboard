b915330b964ce127f583648f5e9a0c8ff21ded2a
ike agbaje <ikea@macbook.local>
2026-06-23 15:50:43 -0400

    updated python code; exports new dataset

--- python/data_cleaning.py
+++ python/data_cleaning.py
@@ -15,19 +15,19 @@ df = df[
 ]
 
 # verifying data was read correctly:
-# print("\n=== First 5 Rows ===")
+# print("\n------ First 5 Rows ------")
 # print(df.head()) # view the first 5 rows
 
-# print("\n=== Dataset Dimensions ===")
+# print("\n------ Dataset Dimensions ------")
 # print(df.shape) # double check dimensions
 
-# print("\n=== Column Names ===")
+# print("\n------ Column Names ------")
 # print(df.columns) # view the column names
 
-# print("\n=== Dataset Info ===")
+# print("\n------ Dataset Info ------")
 # df.info() # view more info about the data
 
-# print("\n=== Missing Values ===")
+# print("\n------ Missing Values ------")
 # print(df.isnull().sum()) # double check for missing values and view how many there are if so
 
 # create maps to assign points to the home and away teams depending on who won using the classic 0/1/3 soccer point standard
@@ -43,10 +43,24 @@ away_points_map = {
     "H": 0
 }
     
-# create additional columns for better analysis
-df["total_goals"] = df["FTHG"] + df["FTAG"] # total combined goals from both teams
-df["goal_difference"] = df["FTHG"] - df["FTAG"] # goal difference; positive value indicates home team won, negative value indicates home team lost
-df["home_points"] = df["FTR"].map(home_points_map) # assign 0, 1, or 3 points to the home team depending on who won using map created earlier
-df["away_points"] = df["FTR"].map(away_points_map) # same as above for away team
-df["Date"] = pd.to_datetime(df["Date"]) # convert the 'Date' column strings to datetime objects 
-df["month"] = df["Date"].dt.month_name() # extract the month; will be useful for filtering data/dashboards
+# feature engineering for better analysis
+df["total_goals"] = df["FTHG"] + df["FTAG"]
+
+# positive = home team scored more goals, negative value = away team scored more goals
+df["goal_difference"] = df["FTHG"] - df["FTAG"]
+
+# assign points based on match outcomes
+df["home_points"] = df["FTR"].map(home_points_map)
+df["away_points"] = df["FTR"].map(away_points_map)
+
+# convert text dates to datetime objects
+df["Date"] = pd.to_datetime(df["Date"])
+
+# extract the month names; will be useful for time-based analysis and filtering data/dashboards
+df["month"] = df["Date"].dt.month_name()
+
+print("\n------ Summary Stats of Data ------")
+print(df[["FTHG", "FTAG", "total_goals"]].describe())
+
+# export cleaned dataset to a new CSV for SQL and power BI
+df.to_csv("../data/cleaned_data.csv", index=False)
